adba0965d20e282778bc82bf6d1470e2282a6ae7
Alex D <alex.spara.dominguez@gmail.com>
2024-02-26 20:03:00 +0000

    Toggle button display property and blue background

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 'use strict';
 
+const buttons = document.querySelectorAll('.modal-btn');
+const modalBtnContainer = document.querySelector('.modal-btn-container');
+
 function recordBtnClick(innerHTML, btnID) {
   let btnModalText = document.querySelector(`#${btnID}-modal-text`);
   btnModalText.style.display = 'inline';
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].style.display = 'none';
+  }
+  modalBtnContainer.style.filter = 'blur(2px)';
 }
 
 function closeModalWindow(parentID) {
   let btnModalText = document.querySelector(`#${parentID}`);
   btnModalText.style.display = 'none';
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].style.display = 'inline';
+  }
+  modalBtnContainer.style.filter = 'none';
 }
