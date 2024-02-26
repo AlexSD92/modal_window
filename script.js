be25b448506ed9c2dac16dad41bf2c6449cdd8f7
Alex D <alex.spara.dominguez@gmail.com>
2024-02-26 19:22:49 +0000

    Create closeModalWindow function to toggle modal window display

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function recordBtnClick(innerHTML, btnID) {
   let btnModalText = document.querySelector(`#${btnID}-modal-text`);
   btnModalText.style.display = 'inline';
 }
+
+function closeModalWindow(parentID) {
+  let btnModalText = document.querySelector(`#${parentID}`);
+  btnModalText.style.display = 'none';
+}
