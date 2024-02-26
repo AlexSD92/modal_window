d1f82affa01cf19afc730abd624dbf28227293b6
Alex D <alex.spara.dominguez@gmail.com>
2024-02-26 20:26:43 +0000

    Basic style added

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 
 const buttons = document.querySelectorAll('.modal-btn');
 const modalBtnContainer = document.querySelector('.modal-btn-container');
+const modalTextContainer = document.querySelector('.modal-text-container');
 
 function recordBtnClick(innerHTML, btnID) {
   let btnModalText = document.querySelector(`#${btnID}-modal-text`);
+  modalTextContainer.style.display = 'block';
   btnModalText.style.display = 'inline';
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].disabled = true;
@@ -14,6 +16,7 @@ function recordBtnClick(innerHTML, btnID) {
 
 function closeModalWindow(parentID) {
   let btnModalText = document.querySelector(`#${parentID}`);
+  modalTextContainer.style.display = 'none';
   btnModalText.style.display = 'none';
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].disabled = false;
