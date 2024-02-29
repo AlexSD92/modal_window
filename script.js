a62d6e12a8d7bd3e60fcc8379423e0d1284581b7
Alex D <alex.spara.dominguez@gmail.com>
2024-02-29 20:19:42 +0000

    Declare blurContainer and modify functions to add and remove .hidden from blurContainer; add event listener for click event on blurContainer

--- script.js
+++ script.js
@@ -3,29 +3,36 @@
 const buttons = document.querySelectorAll('.modal-btn');
 const modalBtnContainer = document.querySelector('.modal-btn-container');
 const modalTextContainer = document.querySelector('.modal-text-container');
+const blurContainer = document.querySelector('.blur');
 let btnModalText;
 
 function recordBtnClick(btnID) {
   btnModalText = document.querySelector(`#${btnID}-modal-text`);
   btnModalText.classList.remove('hidden');
+  blurContainer.classList.remove('hidden');
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].disabled = true;
   }
-  modalBtnContainer.style.filter = 'blur(2px)';
 }
 
 function closeModalWindow(parentID) {
   btnModalText = document.querySelector(`#${parentID}`);
   btnModalText.classList.add('hidden');
+  blurContainer.classList.add('hidden');
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].disabled = false;
   }
-  modalBtnContainer.style.filter = 'none';
 }
 
 document.addEventListener('keydown', (event) => {
   const key = event.key;
   if (key === 'Escape') {
+    closeModalWindow(btnModalText.id);
+  }
+});
+
+document.addEventListener('click', (event) => {
+  if (event.target === blurContainer) {
     closeModalWindow(btnModalText.id);
   }
 });
